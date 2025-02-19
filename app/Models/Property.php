33f8a39894be0e8c67d1b665399219d2909674b0
Hari <haree5bhatta@gmail.com>
2025-02-20 09:02:10 +1100

    test

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Property extends Model
 {
     use HasFactory;
+    // use SoftDeletes;
+    // use if necessary
 
     protected $fillable = [
         'address',
